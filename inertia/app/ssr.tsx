b086ab9a18204b840e5ea8a5665d3c09be25fa0c
Ross Reicks <rreicks13@gmail.com>
2025-02-06 12:55:58 -1000

    Add tailwind css and shadcn

--- inertia/app/ssr.tsx
+++ inertia/app/ssr.tsx
@@ -1,7 +1,7 @@
 import ReactDOMServer from 'react-dom/server'
 import { createInertiaApp } from '@inertiajs/react'
 import type { Page } from '@inertiajs/core'
-
+import '../css/app.css'
 export default function render(page: Page | string) {
     return createInertiaApp({
         page,
